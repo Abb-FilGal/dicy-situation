12942d5e095cfcdd5f35564b6b90ee3d37bbb917
Ahoraas <filipgalin922@gmail.com>
2024-09-06 07:59:12 +0200

    added projectile tag and began work on sawblde

--- Assets/Scripts/Gameplay/Turrets/GenericTurret.cs
+++ Assets/Scripts/Gameplay/Turrets/GenericTurret.cs
@@ -2,26 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SawbladeFrog : MonoBehaviour
+public class GenericTurret : MonoBehaviour
 {
     public int counter = 0;
 
     // Update is called once per frame
     public float waitTime;
     public float raycastLength;
-    public float projectileForce;
-
-    public GameObject projectilePrefab;
 
     IEnumerator PerformActionCoroutine()
     {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(waitTime);
+
         while (true)
         {
-            WaitForSeconds waitForSeconds = new WaitForSeconds(waitTime);
             yield return waitForSeconds;
-  
+
             // Cast a ray forward
             RaycastHit hit;
+            Debug.DrawRay(transform.position, transform.right, Color.green);
             if (Physics.Raycast(transform.position, transform.right, out hit, raycastLength))
             {
                 // Draw a visible ray in the scene view
@@ -44,14 +43,5 @@ public class SawbladeFrog : MonoBehaviour
     void PerformAction()
     {
         Debug.Log("Performing action!");
-
-        // Instantiate the projectile prefab
-        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
-
-        // Get the rigidbody component of the projectile
-        Rigidbody projectileRigidbody = projectile.GetComponent<Rigidbody>();
-
-        // Apply a force to make the projectile move forward
-        projectileRigidbody.AddForce(transform.right * projectileForce, ForceMode.Impulse);
     }
 }

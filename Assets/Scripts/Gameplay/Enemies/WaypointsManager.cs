c9ae8e3d7fded3e622b830ed7c57c686b718240c
svasim <svante.ericsson06@gmail.com>
2024-09-26 09:27:02 +0200

    Changed enemy spawning

--- Assets/Scripts/Gameplay/Enemies/WaypointsManager.cs
+++ Assets/Scripts/Gameplay/Enemies/WaypointsManager.cs
@@ -4,22 +4,25 @@ public class WaypointsManager : MonoBehaviour
 {
     public Transform[] waypoints;
 
-    public Transform GetNextWaypoint(int currentWaypointIndex, bool reverse)
+    public Transform GetNextWaypoint(int currentWaypointIndex, bool reverse = false)
     {
-        if (reverse)
+        if (waypoints != null && currentWaypointIndex >= 0 && currentWaypointIndex < waypoints.Length)
         {
-            if (currentWaypointIndex > 0)
+            if (reverse)
             {
-                return waypoints[currentWaypointIndex - 1];
+                if (currentWaypointIndex > 0)
+                {
+                    return waypoints[currentWaypointIndex - 1];
+                }
+            }
+            else
+            {
+                if (currentWaypointIndex < waypoints.Length - 1)
+                {
+                    return waypoints[currentWaypointIndex + 1];
+                }
             }
         }
-        else
-        {
-            if (currentWaypointIndex < waypoints.Length - 1)
-            {
-                return waypoints[currentWaypointIndex + 1];
-            }
-        }
-        return null; // No more waypoints
+        return null; // No more waypoints or invalid index
     }
 }

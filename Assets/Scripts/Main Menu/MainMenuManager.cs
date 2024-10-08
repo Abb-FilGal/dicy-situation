a7f424195cc87c356fba92fdc21b34b39d4319fc
svasim <svante.ericsson06@gmail.com>
2024-10-08 15:16:02 +0200

    Added some finishing touches

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     public CanvasGroup OptionsPanel;
+    public CanvasGroup InfoPanel;
 
     public void StartGame()
     {
@@ -19,11 +20,27 @@ public class MainMenuManager : MonoBehaviour
         OptionsPanel.interactable = true;
     }
 
+    public void ShowInfo()
+    {
+        InfoPanel.alpha = 1;
+        InfoPanel.blocksRaycasts = true;
+        InfoPanel.interactable = true;
+    }
+
     public void Back()
     {
         OptionsPanel.alpha = 0;
         OptionsPanel.blocksRaycasts = false;
         OptionsPanel.interactable = false;
+
+        InfoPanel.alpha = 0;
+        InfoPanel.blocksRaycasts = false;
+        InfoPanel.interactable = false;
+    }
+
+    public void Website()
+    {
+        Application.OpenURL("https://github.com/Abb-FilGal/dicy-situation");
     }
 
     public void QuitGame()
